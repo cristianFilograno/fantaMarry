f51c4b66b817447dfe3d9b9fa7c6922920a41e38
Cristian Filograno <cristian.filograno@gmail.com>
2023-05-30 11:57:37 +0200

    fix

--- resources/views/regole.blade.php
+++ resources/views/regole.blade.php
@@ -9,15 +9,16 @@
                 <p class="textF textB mb-1 fw-bold lead">REGISTRAZIONE</p>
             </div>
             <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">1.1</strong> Per partecipare al gioco online, gli utenti devono registrarsi fornendo le informazioni richieste dal sistema di registrazione.</p>
-            <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">1.2</strong> Ogni utente può registrare un solo account.</p>
-            <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">1.3</strong> E' possibile visualizzare e modificare il proprio profilo nella sezione 'profilo' in alto a destra.</p>
+            <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">1.4</strong> Non è necessario usare una mail esistente, ma è importante segnarsi i dati di autenticazione poichè non è possibile il recupero di essi.</p>
+            <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">1.3</strong> Ogni utente può registrare un solo account.</p>
+            <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">1.4</strong> E' possibile visualizzare e modificare il proprio profilo nella sezione 'profilo' in alto a destra.</p>
         </div>
         <div class="p-3">
             <div class="d-flex">
                 <div class="text-center lead textF fw-bold circle me-3">2</div>
                 <p class="textF textB mb-1 fw-bold lead">COMPOSIZIONE TEAM</p>
             </div>
-            <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">2.1</strong> Dopo la registrazione, gli utenti devono comporre un team di 5 giocatori (3 dei quali non devono appartenere alla propria cerchia di amici) tra quelli invitati all'evento. I giocatori sono</p>
+            <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">2.1</strong> Dopo la registrazione, gli utenti devono comporre un team di 5 giocatori (3 dei quali non devono appartenere alla propria cerchia di amici) tra quelli invitati all'evento.</p>
             <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">2.2</strong> Una volta scelto il Team non si può più cambiare.</p>
             <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">2.3</strong> E' possibile visualizzare i membri del team e i relativi punteggi nella sezione 'profilo'.</p>
         </div>
@@ -45,7 +46,7 @@
             </div>
             <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">5.1</strong> I team saranno classificati in base al punteggio totale.</p>
             <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">5.2</strong> Al termine dell'evento, il team con il punteggio totale più alto sarà dichiarato vincitore.</p>
-            <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">5.3</strong> In caso di parità di punteggio tra due o più team, il vincitore sarà determinato dall'utente che avrò totalizzato il punteggio maggiore.</p>
+            <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">5.3</strong> In caso di parità di punteggio tra due o più team, il vincitore sarà determinato dall'utente che avrà totalizzato il punteggio maggiore.</p>
             <p class="ms-0 ms-md-5 mb-0 lead"> <strong class="textF textB fw-bold">5.4</strong> In caso di ulteriore parità il vincitore sarà decretato dagli sposi.</p>
         </div>
         <div class="p-3">
