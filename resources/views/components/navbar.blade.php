f8096eccd103a7c3dd852a114f2593ee4dfa82fc
Cristian Filograno <cristian.filograno@gmail.com>
2023-06-01 10:26:42 +0200

    fix

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -22,19 +22,19 @@
                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
               </svg>
             @endif
-            <ul>
-              <li class="nav-item">
+            <ul class="list-unstyled">
+              <li class="nav-item ms-3">
                 <a class="nav-link active anchorNav" href="";>Bentornato {{Auth::user()->name}} </a>
               </li>
               
               <div class="row align-items-center ">
                 <div class="col-7 text-start">
-                  <li><a class="nav-link active anchorNav lead" href="{{route('user.profile', ['userId'=>Auth::user()->id])}}">Profilo</a></li>
-                  <li><a class="nav-link active anchorNav lead" href="{{route('team.create')}}">Crea il tuo Team</a></li>
+                  <li><a class="nav-link active anchorNav lead ms-3" href="{{route('user.profile', ['userId'=>Auth::user()->id])}}">Profilo</a></li>
+                  <li><a class="nav-link active anchorNav lead ms-3" href="{{route('team.create')}}">Crea il tuo Team</a></li>
 
                 </div>
                 <div class="col-3 px-0">
-                  <li class="nav-item">
+                  <li class="nav-item ms-3">
                     <a class="nav-link active anchorNav lead" href="#" onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">
                       <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="red" class="bi bi-box-arrow-left fw-bold" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
